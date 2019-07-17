2fc4a2bac6c5daffd23090031bfa5ff21d24d70a
Lukas Elger <lukaselger01@gmail.com>
2019-07-17 12:48:58 +0200

    Nachrichten-Referenz-Nummer ist nachträglich setzbar

---
+++ lib/ruby_gkv_billing/edifact/message.rb
@@ -0,0 +1,48 @@
+module RubyGkvBilling
+  module Edifact
+    class Message
+
+      def initialize(nachrichten_ref_nummer, nachricht_kennung, segments: [])
+        @segments = segments
+        self.nachrichten_ref_nummer = nachrichten_ref_nummer
+        @nachricht_kennung = nachricht_kennung.to_s[0..3]
+      end
+
+      def nachrichten_ref_nummer=(value)
+        @nachrichten_ref_nummer = value.to_s.rjust(5, "0")[0..4]
+      end
+
+      def nachrichten_ref_nummer
+        @nachrichten_ref_nummer
+      end
+
+      def <<(segment)
+        @segments << segment
+      end
+
+      def header_segment
+        header_segment = RubyGkvBilling::Edifact::Segment.new("UNH")
+        header_segment << @nachrichten_ref_nummer
+        header_segment << @nachricht_kennung + " #{RubyGkvBilling::Edifact::MESSAGE_VERSION} 0 0"
+
+        header_segment
+      end
+
+      def footer_segment
+        footer_segment = RubyGkvBilling::Edifact::Segment.new("UNT")
+        footer_segment << (@segments.size + 2)
+        footer_segment << @nachrichten_ref_nummer
+
+        footer_segment
+      end
+
+      def to_edifact
+        rows = [header_segment]
+        rows += @segments
+        rows << footer_segment
+
+        rows.map(&:to_edifact).join("\n")
+      end
+    end
+  end
+end
---
+++ spec/unit/edifact/message_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe RubyGkvBilling::Edifact::Message do
+  subject { RubyGkvBilling::Edifact::Message.new(123, "SLGA") }
+
+  let(:segment) { RubyGkvBilling::Edifact::Segment.new("UNB", initial_elements: ["Test 123"]) }
+
+  before do
+    segment.<<(-3.14)
+    segment.<<("D'Angelo")
+    segment.<<("Test?")
+
+    subject.<< segment
+  end
+
+  it {
+    expect(subject.header_segment.to_edifact).to eq("UNH+00123+SLGA:12:0:0'")
+  }
+
+  it {
+    expect(subject.footer_segment.to_edifact).to eq("UNT+3+00123'")
+  }
+
+  it {
+    expect(subject.to_edifact).to include("UNH+00123+SLGA:12:0:0'",
+                                          "UNB+Test:123+-3,14+D?'Angelo+Test??'",
+                                          "UNT+3+00123'")
+  }
+end
