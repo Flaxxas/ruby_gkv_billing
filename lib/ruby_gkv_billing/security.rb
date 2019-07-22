ac0ac069ec3f451046f21b57425ee9f3f5f5b80b
Sascha Skulima <sskulima@sektor-n.de>
2019-07-22 11:12:52 +0200

    Links zu den DOCs

--- lib/ruby_gkv_billing/security.rb
+++ lib/ruby_gkv_billing/security.rb
@@ -1,6 +1,8 @@
 require 'ruby_gkv_billing/security/certification'
 require 'ruby_gkv_billing/security/encryption'
 
+## https://www.gkv-datenaustausch.de/media/dokumente/standards_und_normen/technische_spezifikationen/Anlage_16_-_Security-Schnittstelle~1.pdf
+#https://www.gkv-datenaustausch.de/media/dokumente/standards_und_normen/technische_spezifikationen/Best_Practice_Security.pdf
 module RubyGkvBilling
   module Security
   end
