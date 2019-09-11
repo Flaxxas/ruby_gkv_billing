322ef926525d258b683fc164b74f5e96051ee86f
Petrik Bottka <pbottka@sektor-n.de>
2019-09-11 11:35:24 +0200

    Erweteirung der Abfrage von Datenannahmestellen

--- lib/ruby_gkv_billing/provider_file.rb
+++ lib/ruby_gkv_billing/provider_file.rb
@@ -4,8 +4,8 @@ require 'edifact_tools'
 
 module RubyGkvBilling
   class ProviderFile
-    def initialize(file_path)
-      load_file(file_path)
+    def initialize(provider_name)
+      load_file(provider_name)
     end
 
     def dfu_for_client(client_idk, billing_code, tarif_nr = nil)
@@ -24,9 +24,13 @@ module RubyGkvBilling
           vkgs.first["Art_der_Verknüpfung"] == "01" &&
           vkgs.first["IK_des_Verknüpfungspartners"] == mediator_ik
 
-          comm_partner = vkgs.select {|vkg| vkg["Art_der_Verknüpfung"] == "03" && vkg["Abrechnungscode"] == billing_code && vkg["Art_der_Datenlieferung"] == "07"}
-          if comm_partner.length == 1
-            comm_partner_ik = comm_partner.first["IK_des_Verknüpfungspartners"]
+          comm_partner = vkgs.select {|vkg| vkg["Art_der_Verknüpfung"] == "03" && vkg["Art_der_Datenlieferung"] == "07"}
+
+          comm_partner_with_matching_billing_code = comm_partner.select {|vkg| vkg["Abrechnungscode"] == billing_code}
+          comm_partner_with_matching_billing_code = comm_partner.select {|vkg| vkg["Abrechnungscode"] == billing_code[0].concat("0")} if comm_partner_with_matching_billing_code.empty?
+          comm_partner_with_matching_billing_code = comm_partner.select {|vkg| vkg["Abrechnungscode"] == "00"} if comm_partner_with_matching_billing_code.empty?
+          if comm_partner_with_matching_billing_code.length == 1
+            comm_partner_ik = comm_partner_with_matching_billing_code.first["IK_des_Verknüpfungspartners"]
             return communications_for_ik(comm_partner_ik)
           end
         end
@@ -44,12 +48,24 @@ module RubyGkvBilling
       messages.select {|m| m.data_entry("Segment_Identifikation", "Institutionskennzeichen") == nr}.first
     end
 
+    def billing_category
+      EdifactTools::EdifactParser.billing_codes[@data.billing_code]
+    end
+
     def messages
       @messages ||= @data.non_storno_messages
     end
 
-    def load_file(file_path)
-      @data = EdifactTools::EdifactParser.read("spec/examples/bkk.edifact")
+    def load_file(provider_name)
+      @data = EdifactTools::EdifactParser.read(provider_name)
+    end
+
+    def dataname
+      @data.dataname
+    end
+
+    def self.billing_category(billing_code)
+      EdifactTools::EdifactParser.billing_codes[billing_code]
     end
 
     private
--- spec/unit/provider_file_spec.rb
+++ spec/unit/provider_file_spec.rb
@@ -1,8 +1,12 @@
 RSpec.describe RubyGkvBilling::ProviderFile do
-  subject {RubyGkvBilling::ProviderFile.new("spec/examples/bkk.edifact")}
+  subject {RubyGkvBilling::ProviderFile.new("BKK")}
 
   let(:message){subject.search_by_ik("101391700")}
   let(:message2){subject.search_by_ik("101520147")}
+
+  it { expect(RubyGkvBilling::ProviderFile.billing_category("14")).to eq("Hörgeräteakustiker")}
+
+  it {expect(subject.dataname).to eq("BK05Q419ke0")}
 
   it { expect(message).not_to be_nil}
   it { expect(message.data_entry("Segment_Name", "Name-1")).not_to be_nil}
@@ -10,8 +14,8 @@ RSpec.describe RubyGkvBilling::ProviderFile do
   it { expect(message2.data_entry("Segment_Name", "Name-1")).to eq("Shell")}
 
   describe "search for dfu 3 Steps" do
-    it { expect(subject.dfu_for_client("101520147", "40").length).to eq(2)}
-    it { expect(subject.dfu_for_client("101520147", "40")[1]).to eq("ftam.arz-emmendingen.de?:9000")}
+    it { expect(subject.dfu_for_client("101520147", "43").length).to eq(2)}
+    it { expect(subject.dfu_for_client("101520147", "43")[1]).to eq("ftam.arz-emmendingen.de?:9000")}
   end
 
   describe "search for dfu 2 Steps" do
@@ -22,6 +26,16 @@ RSpec.describe RubyGkvBilling::ProviderFile do
   describe "invalid data" do
     it { expect(subject.dfu_for_client("101538013", "71")).to be_nil}
     it { expect(subject.dfu_for_client("101538012", "99")).to be_nil}
+  end
+
+  context "AOK Südlicher Oberrhein" do
+    subject {RubyGkvBilling::ProviderFile.new("AOK")}
+
+    it {expect(subject.dataname).to eq("AO05Q319ke1")}
+
+    it { expect(subject.dfu_for_client("107415518", "00").length).to eq(3)}
+    it { expect(subject.dfu_for_client("107415518", "00").last).to eq("217.110.255.52?:5000")}
+    it { expect(subject.dfu_for_client("107415518", "34").last).to eq("217.110.255.52?:5000")}
   end
 
 end
