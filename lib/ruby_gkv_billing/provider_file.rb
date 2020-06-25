6542630063b0bf3f908ccec3bd6d92febfc93a54
Sascha Skulima <sskulima@sektor-n.de>
2020-06-25 09:08:02 +0200

    prüfe auch 99

--- lib/ruby_gkv_billing/provider_file.rb
+++ lib/ruby_gkv_billing/provider_file.rb
@@ -84,7 +84,10 @@ module RubyGkvBilling
       if references_to_data_recipient && !references_to_data_recipient.empty?
         data_recipient_with_matching_billing_code = references_to_data_recipient.select {|vkg| vkg["Abrechnungscode"] == billing_code} if billing_code && !billing_code.empty?
         data_recipient_with_matching_billing_code = references_to_data_recipient.select {|vkg| vkg["Abrechnungscode"] == billing_code[0].concat("0")} if data_recipient_with_matching_billing_code && data_recipient_with_matching_billing_code.empty?
-        data_recipient_with_matching_billing_code = references_to_data_recipient.select {|vkg| vkg["Abrechnungscode"] == "00"} if data_recipient_with_matching_billing_code && data_recipient_with_matching_billing_code.empty? || billing_code.nil? || (billing_code && billing_code.empty?)
+
+        # 00 oder 99 fuer alle
+        data_recipient_with_matching_billing_code = references_to_data_recipient.select {|vkg| vkg["Abrechnungscode"] == "00" || vkg["Abrechnungscode"] == "99"} if data_recipient_with_matching_billing_code && data_recipient_with_matching_billing_code.empty? || billing_code.nil? || (billing_code && billing_code.empty?)
+
         if data_recipient_with_matching_billing_code.length == 1
           data_recipient_ik = data_recipient_with_matching_billing_code.first["IK_des_Verknüpfungspartners"]
           data_recipient_message = search_by_ik(data_recipient_ik) if data_recipient_ik
@@ -95,7 +98,9 @@ module RubyGkvBilling
         references_to_data_recipient = reference_to_other_provider
         data_recipient_with_matching_billing_code = references_to_data_recipient.select {|vkg| vkg["Abrechnungscode"] == billing_code} if billing_code && !billing_code.empty?
         data_recipient_with_matching_billing_code = references_to_data_recipient.select {|vkg| vkg["Abrechnungscode"] == billing_code[0].concat("0")} if data_recipient_with_matching_billing_code && data_recipient_with_matching_billing_code.empty?
-        data_recipient_with_matching_billing_code = references_to_data_recipient.select {|vkg| vkg["Abrechnungscode"] == "00"} if data_recipient_with_matching_billing_code && data_recipient_with_matching_billing_code.empty? || billing_code.nil? || (billing_code && billing_code.empty?)
+
+        # 00 oder 99 fuer alle
+        data_recipient_with_matching_billing_code = references_to_data_recipient.select {|vkg| vkg["Abrechnungscode"] == "00" || vkg["Abrechnungscode"] == "99"} if data_recipient_with_matching_billing_code && data_recipient_with_matching_billing_code.empty? || billing_code.nil? || (billing_code && billing_code.empty?)
         if data_recipient_with_matching_billing_code.length == 1
           data_recipient_ik = data_recipient_with_matching_billing_code.first["IK_des_Verknüpfungspartners"]
           data_recipient_message = search_by_ik(data_recipient_ik) if data_recipient_ik
